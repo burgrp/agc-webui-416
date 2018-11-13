801ec66dcdbfcf46c9a6c6452987b04eae9061c0
Pavel Burgr <pb@drake.cz>
2018-11-13 21:47:26 +0100

    WIP

--- src/start.js
+++ src/start.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 require("@device.farm/appglue")({require, file: __dirname + "/../config.json", root: require("@device.farm/webglue")}).main(async config => {
     console.info("AGC UI ready");
 });
